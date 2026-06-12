568ebbc2798fe0c77952a4a6c2d88a0cc897af9d
Egor Manko <mankoegor5@gmail.com>
2026-06-12 17:32:41 +0300

    feat: refactor ColumnBoard component to streamline task deletion, enhance UI structure, and improve error handling

--- src/components/board/ColumnBoard/ColumnBoard.tsx
+++ src/components/board/ColumnBoard/ColumnBoard.tsx
@@ -95,13 +95,6 @@ function ColumnBoard({column, tasks, deleteColumn, updateColumn, isUpdated}: Col
         }
     }
 
-    const handleDeleteTask = async (taskId: string) => {
-        try {
-            await deleteTask(taskId);
-        } catch (err: any) {
-            setLocalError(err instanceof Error ? err : new Error(String(err)));
-        }
-    }
 
     const {ref, isDropTarget} = useDroppable({
         id: column.id,
@@ -117,73 +110,73 @@ function ColumnBoard({column, tasks, deleteColumn, updateColumn, isUpdated}: Col
     return(
         <>
         
+        <div className={s.column}>
+            <div className={s.header}> 
+                <h1 className={s.title}>{column.title}</h1>
+                <div className={s.menuActionDiv}>
+                    <button className={s.actionButton} onClick={() => deleteColumn(column.id)}>
+                        <img className={s.btnIcon} src={deleteIcon} alt="Delete" />
+                    </button>
+                    <button className={s.actionButton} onClick={() => setColumnIsUpdateClicked(true)}>
+                        <img className={s.btnIcon} src={editIcon} alt="Rename" />
+                    </button>
+                </div>
+            </div>
+
+            {isColumnUpdateClicked && <CreateModalWindow    
+                                            headerTitle="Update Column Title"
+                                            labelText="COLUMN TITLE"
+                                            setIsClicked={setColumnIsUpdateClicked}
+                                            setTitle={setUpdateColumnTitle}
+                                            title={updateColumnTitle}
+                                            isDoneState={isUpdated}
+                                            heandleCreate={handleUpdateColumnTitle}
+                                            prevTitle={column.title}/>}
+
+
+            <SortableContext 
+                items={tasks.map(task => task.id)} 
+                strategy={verticalListSortingStrategy}>
+                <div ref={ref} className={s.content}>
+                {tasks.map((task: Task, index: number) => (
+                    <TaskCard
+                        key={task.id}
+                        task={task}
+                        index={index}
+                        deleteTask={deleteTask}
+                        isDeleted={isDeleted}
+                        />
+                ))}
+
+                {isDropTarget && tasks.length === 0 && <div className={s.dropDiv}>DROP IT HERE</div>}
+            </div>
+            </SortableContext>
+
+
+            <button className={s.addButton} onClick={() => setIsClicked(true)}>
+                <img className={s.buttonIcon} src={addIcon} alt="" />
+                <p className={s.buttonText}>Add Task</p>
+            </button>
+
             {localError && <ErrorModalWindow error={localError}/>}
 
-            <div className={s.column}>
-                <div className={s.header}> 
-                    <h1 className={s.title}>{column.title}</h1>
-                    <div className={s.menuActionDiv}>
-                        <button className={s.actionButton} onClick={() => handleDeleteTask(column.id)}>
-                            <img className={s.btnIcon} src={deleteIcon} alt="Delete" />
-                        </button>
-                        <button className={s.actionButton} onClick={() => setColumnIsUpdateClicked(true)}>
-                            <img className={s.btnIcon} src={editIcon} alt="Rename" />
-                        </button>
-                    </div>
-                </div>
-
-                {isColumnUpdateClicked && <CreateModalWindow    
-                                                headerTitle="Update Column Title"
-                                                labelText="COLUMN TITLE"
-                                                setIsClicked={setColumnIsUpdateClicked}
-                                                setTitle={setUpdateColumnTitle}
-                                                title={updateColumnTitle}
-                                                isDoneState={isUpdated}
-                                                heandleCreate={handleUpdateColumnTitle}
-                                                prevTitle={column.title}/>}
-
-
-                <SortableContext 
-                    items={tasks.map(task => task.id)} 
-                    strategy={verticalListSortingStrategy}>
-                    <div ref={ref} className={s.content}>
-                    {tasks.map((task: Task, index: number) => (
-                        <TaskCard
-                            key={task.id}
-                            task={task}
-                            index={index}
-                            deleteTask={deleteTask}
-                            isDeleted={isDeleted}
-                            />
-                    ))}
-
-                    {isDropTarget && tasks.length === 0 && <div className={s.dropDiv}>DROP IT HERE</div>}
-                </div>
-                </SortableContext>
-
-
-                <button className={s.addButton} onClick={() => setIsClicked(true)}>
-                    <img className={s.buttonIcon} src={addIcon} alt="" />
-                    <p className={s.buttonText}>Add Task</p>
-                </button>
-
-                {isClicked && <CreateTaskModalWindow
-                                    title={taskTitle}
-                                    setTitle={setTaskTitle}
-                                    description={taskDescription}
-                                    setDescription={setTaskDescription}
-                                    priority={taskPriority}
-                                    setPriority={setTaskPriority}
-                                    dueDate={dueDate}
-                                    setDueDate={setDueDate}
-                                    // assigneeId={assigneeId}
-                                    // setAssigneeId={setAssigneeId}
-                                    setIsClicked={setIsClicked}
-                                    isCreating={isCreated}
-                                    heandleCreateTask={handleCreateTask}
-                                    />}
-            </div>
-        </>
+            {isClicked && <CreateTaskModalWindow
+                                title={taskTitle}
+                                setTitle={setTaskTitle}
+                                description={taskDescription}
+                                setDescription={setTaskDescription}
+                                priority={taskPriority}
+                                setPriority={setTaskPriority}
+                                dueDate={dueDate}
+                                setDueDate={setDueDate}
+                                // assigneeId={assigneeId}
+                                // setAssigneeId={setAssigneeId}
+                                setIsClicked={setIsClicked}
+                                isCreating={isCreated}
+                                heandleCreateTask={handleCreateTask}
+                                />}
+        </div>
+    </>
     )
 }
 
